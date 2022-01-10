65a6220759e333d23de5202d88b5176bc2fe4cc6
Alston Fernandes <89850018+05Alston@users.noreply.github.com>
2022-01-10 23:20:01 +0530

    Separated contact page

--- intro.js
+++ intro.js
@@ -1,12 +1,12 @@
 var intro = document.querySelector(".intro");
 var social = document.querySelector(".social")
-
+window.scrollTo(0,0);
 document.body.style.overflow = 'hidden';
 window.addEventListener("DOMContentLoaded", () => {
     setTimeout(() => {
-        setTimeout(() => { social.style.opacity = "1.0"; document.body.style.overflow = ''; }, 700)
+        // setTimeout(() => { social.style.opacity = "1.0"; document.body.style.overflow = ''; }, 800)
         console.log("Hello");
-        intro.style.height = "0";
+        // intro.style.height = "0";
     }, 1400);
 
 });
